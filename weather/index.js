8806205d797a3b4a8af1d7162e755c6856008b2f
yee2542 <yee2542gtl@gmail.com>
2019-02-01 00:59:31 +0700

    add : console input

--- weather/index.js
+++ weather/index.js
@@ -1,18 +1,15 @@
 // dependencies
 const fs = require('fs')
-const util = require('util')
 
 const xmlToJson = require('xml-to-json-stream')
-const es = require('event-stream')
-const JSONStream = require('JSONStream')
 const format = require('format-json-stream')
+
+const inputPath = process.argv[2]
+const outputPath = process.argv[3]
 
 const parser = xmlToJson()
 const streamConvert = parser.createStream()
-const streamRead = fs.createReadStream('./weather.xml', { encoding: 'utf8' })
-const streamWrite = fs.createWriteStream('jsonFile.json')
-// streamRead.on('data', data => {
-//   console.log(data)
-//   streamConvert.pipe(data)
-// })
+const streamRead = fs.createReadStream(inputPath, { encoding: 'utf8' })
+const streamWrite = fs.createWriteStream(outputPath)
+
 streamRead.pipe(streamConvert).pipe(format()).pipe(streamWrite)
